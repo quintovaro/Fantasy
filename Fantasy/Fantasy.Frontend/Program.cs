6f55c1a0769d3d5144d0127d67d54e1286b31a4d
Octavio Cienfuegos Gomez <ozagal@quintovaro.com>
2024-10-04 17:52:19 +0200

    Creando país

--- Fantasy/Fantasy.Frontend/Program.cs
+++ Fantasy/Fantasy.Frontend/Program.cs
@@ -1,3 +1,4 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using Fantasy.Frontend;
 using Fantasy.Frontend.Repositories;
 using Microsoft.AspNetCore.Components.Web;
@@ -9,5 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7108") });
 builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddLocalization();
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
--- Fantasy/Fantasy.Frontend/Repositories/Repository.cs
+++ Fantasy/Fantasy.Frontend/Repositories/Repository.cs
@@ -17,6 +17,12 @@ public class Repository : IRepository
         PropertyNameCaseInsensitive = true,
     };
 
+    public async Task<HttpResponseWrapper<object>> DeleteAsync<T>(string url)
+    {
+        var responseHttp = await _httpClient.DeleteAsync(url);
+        return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
+    }
+
     public async Task<HttpResponseWrapper<T>> GetAsync<T>(string url)
     {
         var responseHttp = await _httpClient.GetAsync(url);
@@ -27,12 +33,6 @@ public class Repository : IRepository
         }
 
         return new HttpResponseWrapper<T>(default, true, responseHttp);
-    }
-
-    private async Task<T> UnserializeAnswer<T>(HttpResponseMessage responseHttp)
-    {
-        var response = await responseHttp.Content.ReadAsStringAsync();
-        return JsonSerializer.Deserialize<T>(response, _jsonDefaultOptions)!;
     }
 
     public async Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model)
@@ -55,5 +55,33 @@ public class Repository : IRepository
         }
 
         return new HttpResponseWrapper<TActionResponse>(default, !responseHttp.IsSuccessStatusCode, responseHttp);
+    }
+
+    public async Task<HttpResponseWrapper<object>> PutAsync<T>(string url, T model)
+    {
+        var messageJSON = JsonSerializer.Serialize(model);
+        var messageContet = new StringContent(messageJSON, Encoding.UTF8, "application/json");
+        var responseHttp = await _httpClient.PutAsync(url, messageContet);
+        return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
+    }
+
+    public async Task<HttpResponseWrapper<TActionResponse>> PutAsync<T, TActionResponse>(string url, T model)
+    {
+        var messageJSON = JsonSerializer.Serialize(model);
+        var messageContet = new StringContent(messageJSON, Encoding.UTF8, "application/json");
+        var responseHttp = await _httpClient.PutAsync(url, messageContet);
+        if (responseHttp.IsSuccessStatusCode)
+        {
+            var response = await UnserializeAnswer<TActionResponse>(responseHttp);
+            return new HttpResponseWrapper<TActionResponse>(response, false, responseHttp);
+        }
+
+        return new HttpResponseWrapper<TActionResponse>(default, !responseHttp.IsSuccessStatusCode, responseHttp);
+    }
+
+    private async Task<T> UnserializeAnswer<T>(HttpResponseMessage responseHttp)
+    {
+        var response = await responseHttp.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<T>(response, _jsonDefaultOptions)!;
     }
 }
